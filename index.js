4b82009050c64144e254a1af69b44ec0b8a9150d
a7mad1112 <ahmalawneh79@gmail.com>
2023-08-07 18:04:01 +0200

    task 1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const users = require("./users.js");
+// Task 1: Find the person with the most skills in the users object and print their name
+const findPersonMostSkills = (users) => {
+  const names = Object.keys(users);
+  // because the name is a key, we can store the key and the objects in array
+  let maxPerson = [names[0], users[names[0]]];
+  for (const key in users) {
+    const userSkillsLength = users[key].skills.length;
+    const maxPersonSkillsLength = maxPerson[1].skills.length;
+    if (userSkillsLength > maxPersonSkillsLength) {
+      maxPerson = [key, users[key]];
+    }
+  }
+  return maxPerson;
+};
+
+const mostSkilledPerson = findPersonMostSkills(users);
+console.log(`The person with the most skills is: ${mostSkilledPerson[0]}`);
