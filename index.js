5231f0d07107c14aeaa03a682766f2fac2497deb
a7mad1112 <ahmalawneh79@gmail.com>
2023-08-07 18:04:51 +0200

    task 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const users = require("./users.js");
 // Task 1: Find the person with the most skills in the users object and print their name
+/*
 const findPersonMostSkills = (users) => {
   const names = Object.keys(users);
   // because the name is a key, we can store the key and the objects in array
@@ -16,3 +17,10 @@ const findPersonMostSkills = (users) => {
 
 const mostSkilledPerson = findPersonMostSkills(users);
 console.log(`The person with the most skills is: ${mostSkilledPerson[0]}`);
+*/
+
+
+// Task 2: Count the number of logged-in users and users with points greater than or equal to 50, then print both counts.
+
+const loggedUsersCount = Object.keys(users).reduce((acc, curr) => users[curr].isLoggedIn && users[curr].points >= 50 ? acc + 1 : acc, 0)
+console.log("Logged Person Count: " + loggedUsersCount)
